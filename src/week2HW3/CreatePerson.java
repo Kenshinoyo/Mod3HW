bb2fdeee9643c7e62498c13c67f8b2385a3802eb
Garrad Belle <garrad.d.belle@gmail.com>
2021-10-11 22:32:39 -0400

    person 1 class

---
+++ src/week2HW3/CreatePerson.java
@@ -0,0 +1,12 @@
+package week2HW3;
+
+public class CreatePerson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		CreateStudent(Person obj);
+
+	}
+
+}
---
+++ src/week2HW3/Pesron1.java
@@ -0,0 +1,40 @@
+package week2HW3;
+
+// 4)
+
+public class Pesron1 {
+	
+	private String Species, Build, Birth Year;
+	private int fullAge, weight = 0;
+	public String getSpecies() {
+		return Species;
+	}
+	public void setSpecies(String species) {
+		Species = species;
+	}
+	public String getBuild() {
+		return Build;
+	}
+	public void setBuild(String build) {
+		Build = build;
+	}
+	public String getBirth() {
+		return Birth;
+	}
+	public void setBirth(String birth) {
+		Birth = birth;
+	}
+	public int getFullAge() {
+		return fullAge;
+	}
+	public void setFullAge(int fullAge) {
+		this.fullAge = fullAge;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+}
